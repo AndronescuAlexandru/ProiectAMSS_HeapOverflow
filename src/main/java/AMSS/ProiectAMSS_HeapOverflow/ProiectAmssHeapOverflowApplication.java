0254f7803e5ae99cbf2516f5ca6e3e7dcf17fb6d
Andronescu Alexandru <alex_2001_22@yahoo.ro>
2023-11-27 19:53:35 +0200

    Initial commit

---
+++ src/main/java/AMSS/ProiectAMSS_HeapOverflow/ProiectAmssHeapOverflowApplication.java
@@ -0,0 +1,13 @@
+package AMSS.ProiectAMSS_HeapOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectAmssHeapOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectAmssHeapOverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AMSS/ProiectAMSS_HeapOverflow/ProiectAmssHeapOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package AMSS.ProiectAMSS_HeapOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectAmssHeapOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
